6e25a940f0e743cd7c89b604855e2e3f41e037c8
Steve Deemer <it@xcsat.com>
2024-10-21 19:02:18 -0400

    style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,12 @@
         <!-- Google Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Bootstrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
         <!-- Vendor CSS Files -->
         <link href="/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
         <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -40,13 +46,9 @@
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- Bootstrap JS -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
         <style>
+
             footer h5 {
                 margin-bottom: 20px;
                 font-weight: bold;
@@ -91,6 +93,7 @@
                     opacity: 1;
                 }
             }
+
         </style>
     </head>
     <body class="font-sans antialiased">
