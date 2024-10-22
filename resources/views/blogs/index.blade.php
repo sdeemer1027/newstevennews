a3e62e23b634a2da9b502311e65f25636d75b1c7
Steve Deemer <it@xcsat.com>
2024-10-22 12:19:47 -0400

    style

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -1,22 +1,21 @@
 <x-app-layout>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight ">
             Blogs
         </h2>
     </x-slot>
 
     <div class="py-3">
 
-        <div class="w-full mx-auto px-4">
-            <div class="bg-gray-100 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="w-full mx-auto px-4 ">
+            <div class="bg-gray-100 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg bg-black">
                 <div class="p-6 text-white dark:text-white">
                     <a href="{{route('home')}}">Home</a> -> Blogs
                     <div class="container-fluid my-5">
                         <div class="row">
                             <div class="col-md-3">
                                 <div class="about-me">
-                                    <button id="dark-mode-toggle" onclick="toggleDarkMode()" class="btn btn-primary">Toggle Dark Mode</button>
                                     <h3>Categories</h3>
                                     @include('blogs.categorylist')
                                 </div>
--- resources/views/foodfact/index.blade.php
+++ resources/views/foodfact/index.blade.php
@@ -7,9 +7,9 @@
 <BR>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-black dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-black dark:text-gray-100">
-                   
+
 
 <h1>{{ $product['product_name'] ?? ''}}</h1>
 
@@ -64,7 +64,7 @@
 <BR><BR><BR>
 <HR>
 
-{{$product}}
+{{--$product--}}
 
 
 
