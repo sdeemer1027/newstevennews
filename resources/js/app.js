85fd014b3e39b11d2ea5af58d9d77bfda289d9bd
Steve Deemer <it@xcsat.com>
2024-10-24 21:09:01 -0400

    manually adding tinymce

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,7 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import tinymce from 'tinymce/tinymce';
-import 'tinymce/themes/silver';
-import 'tinymce/icons/default';
-import 'tinymce/plugins/code';
+
 window.Alpine = Alpine;
 
 Alpine.start();
-
-document.addEventListener('DOMContentLoaded', () => {
-    const editorElements = document.querySelectorAll('textarea.tinymce-editor');
-    editorElements.forEach((editorElement) => {
-        if (editorElement) {
-            import('tinymce/tinymce').then(tinymce => {
-                console.log('TinyMCE loaded'); // Add this line
-                tinymce.init({
-
-                    selector: editorElement,
-                    plugins: 'code',
-                    toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | code',
-                    height: 400,
-                });
-            });
-        }
-    });
-});
