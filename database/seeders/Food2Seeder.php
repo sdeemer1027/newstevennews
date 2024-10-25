529423752747b8a47db1df5e674c4e2db59b5da6
Steve Deemer <it@xcsat.com>
2024-10-25 05:22:59 -0400

    seeder

--- database/seeders/Food2Seeder.php
+++ database/seeders/Food2Seeder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class Food2Seeder extends Seeder
+class FoodSeeder extends Seeder
 {
     /**
      * Run the database seeds.
