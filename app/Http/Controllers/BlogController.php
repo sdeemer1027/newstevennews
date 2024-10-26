d107c8c0cc9ce29c0df1dafe5751555806aaa9f8
Steve Deemer <it@xcsat.com>
2024-10-25 20:53:28 -0400

    linkedin post

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -29,7 +29,7 @@ class BlogController extends Controller
         $request->validate([
             'title' => 'required|string|max:255',
             'slug' => 'required|string|max:255',
-            'content' => 'required|string',
+            'content' => 'required',
             'categories' => 'required|array',
         ]);
 
